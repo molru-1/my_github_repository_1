44f6a290176e5e0d5ec1813ef793912141a191cc
김준우 <gimalidjqp@gmail.com>
2026-08-29 22:10:04 +0900

    EZ

--- week2/1. basic/08_bubble_sort.py
+++ week2/1. basic/08_bubble_sort.py
@@ -41,6 +41,12 @@ def bubble_sort(arr):
     ## TODO: 인접한 두 원소 비교 및 교환
     ## arr[j] > arr[j+1]이면 교환
     ## 외부 반복문: n-1번 실행
+    for i in range(0,n):
+        for j in range(0, i+1):
+            if(arr[i]<arr[j]):
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
     pass
         
     return arr
@@ -61,7 +67,16 @@ def bubble_sort_optimized(arr):
         swapped = False  # 교환 발생 여부
         
         # TODO: 내부 반복문과 교환 로직 구현
-        # 교환이 발생하면 swapped = True 설정        
+        # 교환이 발생하면 swapped = True 설정
+        for i in range(0,n):
+            for j in range(0, i+1):
+                if(arr[i]<arr[j]):
+                    swapped = True
+                    temp = arr[i]
+                    arr[i] = arr[j]
+                    arr[j] = temp 
+                if(swapped == False and j>= i+1):
+                    break
         pass
         
 
