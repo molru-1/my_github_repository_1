655431ea579c96d5883a2e39a73ba419b16741c1
김준우 <gimalidjqp@gmail.com>
2026-09-04 21:17:38 +0900

    5

--- week3/1. basic/04_bfs.py
+++ week3/1. basic/04_bfs.py
@@ -46,13 +46,21 @@ def bfs(graph, start):
     visited = []
     
     # TODO: 큐 생성 및 시작 정점 추가
+    Q = deque([start])
+
     ## 방문한 정점 집합
+    visited = [start]
     pass
 
     # TODO: 큐가 빌 때까지 반복
+    while Q:
+        node = Q.popleft()
     ## 큐에서 정점 꺼내기
-    ## 인접한 정점들 확인
-    ## 방문하지 않은 정점이면 큐에 추가
+        for neighbor in graph[node]:
+            ## 방문하지 않은 정점이면 큐에 추가
+            if neighbor not in visited:
+                visited.append(neighbor)
+                Q.append(neighbor)
     pass
     
     return visited
--- week3/1. basic/05_dfs.py
+++ week3/1. basic/05_dfs.py
@@ -44,11 +44,16 @@ def dfs(graph, start, visited=None):
     """
     # TODO: visited가 None이면 초기화
     pass
-    
+    if visited is None:
+        visited = []
     # TODO: 현재 정점 방문
+    visited.append(start)
     pass
     
     # TODO: 인접한 정점들에 대해 재귀
+    for i in graph[start]:
+        if i not in visited:
+            dfs(graph, i, visited)
     ## 방문하지 않은 정점이면 재귀 호출
     pass
     
