361bf3ec88453ee58b34b901b16b760bd470b489
김준우 <gimalidjqp@gmail.com>
2026-09-04 13:15:33 +0900

    week3 - 3

--- week3/1. basic/03_graph_basic.py
+++ week3/1. basic/03_graph_basic.py
@@ -45,12 +45,15 @@ def create_graph(vertices, edges, directed=False):
 
     for i in range(vertices):
         graph[i] = []
-        
     pass
     
     # TODO: 간선 추가
     ## 간선 추가 (u에서 v로)
+    for u,v in edges:
+        graph[u].append(v)
     ## 무방향 그래프면 반대 방향도 추가
+        if directed == False:
+            graph[v].append(u)
     pass
     
     return graph
